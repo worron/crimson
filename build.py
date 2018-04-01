f4b4f6c739df4715c40466a34a0d57660d9bfc55
worron <worrongm@gmail.com>
2018-04-02 02:33:54 +0300

    Add logger

--- build.py
+++ build.py
@@ -3,11 +3,42 @@
 import os
 import sys
 import subprocess
+import logging
 
 from argparse import ArgumentParser
 from tempfile import NamedTemporaryFile
 
 
+# Logger setup
+CI = dict(zip(("BLACK", "RED", "GREEN", "YELLOW", "BLUE", "MAGENTA", "CYAN", "WHITE"), range(8)))
+COLORS = {'WARNING': CI["YELLOW"], 'INFO': CI["GREEN"], 'DEBUG': CI["BLUE"], 'CRITICAL': CI["RED"], 'ERROR': CI["RED"]}
+
+RESET_SEQ = "\033[0m"
+COLOR_SEQ = "\033[0;%dm"
+BOLD_SEQ = "\033[1m"
+
+COLOR_PACK = list(('$' + color, COLOR_SEQ % (30 + CI[color])) for color in CI.keys())
+MESSAGE_PATTERN = "$COLOR$BOLD%(levelname)s $RESET$COLOR(L%(lineno)d): $RESET%(message)s"
+
+
+class ColoredFormatter(logging.Formatter):
+	def format(self, record):
+		level_color = COLOR_SEQ % (30 + COLORS[record.levelname])
+		message = super().format(record)
+		for rep in [('$RESET', RESET_SEQ), ('$BOLD', BOLD_SEQ), ('$COLOR', level_color)] + COLOR_PACK:
+			message = message.replace(*rep)
+		return message + RESET_SEQ
+
+
+logger = logging.getLogger(__name__)
+color_formatter = ColoredFormatter(MESSAGE_PATTERN)
+stream_handler = logging.StreamHandler(stream=sys.stdout)
+
+stream_handler.setFormatter(color_formatter)
+logger.addHandler(stream_handler)
+
+
+# Script environment setup
 _base_path = os.path.dirname(os.path.abspath(__file__))
 _pattern_path = os.path.join(_base_path, 'patterns')
 _theme_path = os.path.join(_base_path, 'crimson')
@@ -18,17 +49,20 @@ _font_list = (
 )
 
 
+# Script functions
 def _run(cmd):
 	try:
 		subprocess.call(cmd)
+		logger.debug("Successfully executed:\n{0}".format(' '.join(cmd)))
 	except Exception as e:
-		print("Fail to run command\n{0}\n\n{1}".format(' '.join(cmd), e))
-		sys.exit()
+		logger.error("Fail to run command\n{0}\n\n{1}".format(' '.join(cmd), e))
+		sys.exit(1)
 
 
 def _create_dir(dir_):
 	if not os.path.exists(dir_):
 		os.makedirs(dir_)
+		logger.info("New folder created:\n{0}".format(dir_))
 
 
 def build_images(options):
@@ -41,6 +75,7 @@ def build_images(options):
 
 	for root, dirs, files in os.walk(_pattern_path):
 		for file_ in filter(lambda f: f.endswith('.pat'), files):
+			logger.debug("Creating image from pattern '{0}'".format(file_))
 			with open(os.path.join(root, file_), 'r') as source_, open(tmp_file.name, 'r+') as output_:
 				txt = source_.read()
 				for k, v in replacements.items():
@@ -63,6 +98,7 @@ def build_config(options):
 		'$second-color': options.second_color,
 	}
 
+	logger.debug("Creating theme file from pattern")
 	config_pattern = os.path.join(_pattern_path, 'theme.tpt')
 	config_file = os.path.join(_theme_path, 'theme.txt')
 
@@ -77,7 +113,7 @@ def build_background(image):
 	if image is not None and os.path.isfile(image):
 		background = image
 	else:
-		print("Using default background")
+		logger.warning("No background image specified, using console background color as fallback")
 		background = os.path.join(_theme_path, 'terminal_bg.png')
 
 	_run(['cp', background, os.path.join(_theme_path, 'background.png')])
@@ -85,6 +121,7 @@ def build_background(image):
 
 def build_fonts(font_list):
 	for font in font_list:
+		logger.debug("Converting font '{0}'".format(font['file']))
 		input_file = os.path.join(_pattern_path, font['file'])
 		output_file = os.path.join(_theme_path, font['name'] + '.pf2',)
 
@@ -93,7 +130,6 @@ def build_fonts(font_list):
 
 
 def build_theme(options):
-	print(options)
 	_create_dir(_theme_path)
 
 	build_images(options)
@@ -108,6 +144,7 @@ def parse_command_line():
 	parser.add_argument('--main-color', '-m', metavar='RRGGBB', default='CB252D', help='Main theme color')
 	parser.add_argument('--second-color', '-s', metavar='RRGGBB', default='CCCCCC', help='Second theme color')
 	parser.add_argument('--bg-color', '-b', metavar='RRGGBB', default='1A1A1A', help='Background theme color')
+	parser.add_argument('--log-level', '-l', metavar='LEVEL', default='INFO', help='Log level')
 
 	options = parser.parse_args()
 
@@ -119,4 +156,6 @@ def parse_command_line():
 
 if __name__ == "__main__":
 	args = parse_command_line()
+	logger.setLevel(logging.getLevelName(args.log_level))
+
 	build_theme(args)
